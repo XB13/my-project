59c58e0c11857f142cf706f44522f76d1ad20ee5
XB13 <80102401+XB13@users.noreply.github.com>
2021-08-12 04:45:29 +0200

    Update hacl.py

--- hacl.py
+++ hacl.py
@@ -1,3 +1,25 @@
 import os
 cl="start"
 os.system(cl)
+
+cls="start"
+os.system(cls)
+
+cdl="start"
+os.system(cdl)
+
+clg="start"
+os.system(clg)
+
+cfl="start"
+os.system(cfl)
+
+
+cla="start"
+os.system(cla)
+
+clff="start"
+os.system(clff)
+
+czl="start"
+os.system(czl)
